929fb649bad7760bf633900a64dbe10292264329
Subham Kumar Singh <subhamsingh225500@gmail.com>
2020-10-02 19:48:26 +0530

    Update Logic.java

--- Week 2/m4-assignment-diamond-sizes-skeleton/app/src/main/java/mooc/vandy/java4android/diamonds/logic/Logic.java
+++ Week 2/m4-assignment-diamond-sizes-skeleton/app/src/main/java/mooc/vandy/java4android/diamonds/logic/Logic.java
@@ -45,71 +45,51 @@ public class Logic
      * on-screen button labeled 'Process...' is pressed.
      */
     public void process(int size) {
-        int height = size * 2 + 1;
-        int width = size * 2 + 2;
-        int accumulator = -(size+1);
-
-        for(int i=1;i<=height;i++){
-            accumulator++;
-            for (int j=1;j<=width;j++) {
-                if((i == 1 || i == height) && (j == 1 || j == width))
-                    mOut.print("+");
-                else if((i == 1 || i == height) && !(j == 1 || j == width))
+         int n=size;
+        mOut.print("+");
+        for(int i=1;i<=n*2;i++)           //prints the number - in between two +
+            mOut.print("-");
+        mOut.println("+");
+        for(int i=1;i<n*2;i++)
+        {
+            mOut.print("|");              
+            for(int j=1;j<=n-i;j++)
+                mOut.print(" ");
+            for(int j=1;j<=i-n;j++)       
+                mOut.print(" ");
+            if(i<n)
+                mOut.print("/");
+            else if(i==n)
+                mOut.print("<");
+            else
+                mOut.print("\\");
+            int x=i;
+            if(i>n)
+                x=2*n-i;
+            x=x*2-2;
+            for(int j=1;j<=x;j++)
+            {
+                if(i%2==0)
                     mOut.print("-");
-                else if(!(i == 1 || i == height) && (j == 1 || j == width))
-                    mOut.print("|");
-                else {
-                    drawDiamond(size, i, j, accumulator);
-                }
-            }
-            mOut.print("\n");
-        }
-    }
-
-    public void drawDiamond(int size, int i, int j, int accumulator){
-        int diamondRowThickness;
-        if (accumulator <= 0){
-            diamondRowThickness = i*2-2;
-        } else {
-            diamondRowThickness = (i-accumulator*2)*2-2;
-        }
-        int diamondMidpoint = size + 1;
-        int diamondBoundsLeft = diamondMidpoint - (diamondRowThickness/2-1);
-        int diamondBoundsRight = diamondMidpoint + (diamondRowThickness/2);
-        int frameTop = 1;
-        int frameBottom = size * 2 + 1;
-
-        if (j >= diamondBoundsLeft && j <= diamondBoundsRight) {
-            if (j == diamondBoundsLeft || j == diamondBoundsRight) {
-                if (i < diamondMidpoint && i > frameTop) {
-                    if (j == diamondBoundsLeft) {
-                        mOut.print("/");
-                    } else {
-                        mOut.print("\\");
-                    }
-                } else if (i == diamondMidpoint) {
-                    if (j == diamondBoundsLeft) {
-                        mOut.print("<");
-                    } else {
-                        mOut.print(">");
-                    }
-                } else if (i > diamondMidpoint && i < frameBottom) {
-                    if (j == diamondBoundsLeft) {
-                        mOut.print("\\");
-                    } else {
-                        mOut.print("/");
-                    }
-                }
-            } else {
-                if (i % 2 == 0) {
+                else
                     mOut.print("=");
-                } else {
-                    mOut.print("-");
-                }
             }
-        } else {
-            mOut.print(" ");
+            if(i<n)
+                mOut.print("\\");
+            else if(i==n)
+                mOut.print(">");
+            else
+                mOut.print("/");
+            for(int j=1;j<=n-i;j++)
+                mOut.print(" ");
+            for(int j=1;j<=i-n;j++)
+                mOut.print(" ");
+            mOut.println("|");
         }
+        mOut.print("+");
+        for(int i=1;i<=n*2;i++)
+            mOut.print("-");
+        mOut.println("+");
     }
 
 }
